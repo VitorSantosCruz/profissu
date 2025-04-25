0f3db7231ada7d194805557b5c435223bac8514d
Vítor Cruz <vittorcruz18101998@gmail.com>
2025-04-25 01:45:12 -0300

    feat: implement entrypoint to list offers

--- src/main/java/br/com/conectabyte/profissu/mappers/ConversationMapper.java
+++ src/main/java/br/com/conectabyte/profissu/mappers/ConversationMapper.java
@@ -3,6 +3,8 @@ package br.com.conectabyte.profissu.mappers;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 import br.com.conectabyte.profissu.dtos.request.ConversationRequestDto;
 import br.com.conectabyte.profissu.dtos.response.ConversationResponseDto;
@@ -23,4 +25,14 @@ public interface ConversationMapper {
   Conversation conversationRequestDtoToConversation(ConversationRequestDto conversationRequestDto);
 
   ConversationResponseDto conversationToConversationResponseDto(Conversation conversation);
+
+  default Page<ConversationResponseDto> conversationPageToConversationResponseDtoPage(
+      Page<Conversation> conversationPage) {
+    final var conversationResponseDtoPageContent = conversationPage.getContent().stream()
+        .map(this::conversationToConversationResponseDto)
+        .toList();
+
+    return new PageImpl<>(conversationResponseDtoPageContent, conversationPage.getPageable(),
+        conversationPage.getTotalElements());
+  }
 }
--- src/main/java/br/com/conectabyte/profissu/repositories/ConversationRepository.java
+++ src/main/java/br/com/conectabyte/profissu/repositories/ConversationRepository.java
@@ -1,8 +1,13 @@
 package br.com.conectabyte.profissu.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.conectabyte.profissu.entities.Conversation;
 
 public interface ConversationRepository extends JpaRepository<Conversation, Long> {
+  @Query("FROM Conversation c WHERE c.requester.id = :userId")
+  Page<Conversation> findByUserId(Long userId, Pageable pageable);
 }
